6bca4fab69b3519542cb05a08d888299a934f560
sameer11001 <sameeralhajeid.prog@gmail.com>
2024-10-30 01:35:52 +0300

    add flag with image and add flag isLike and return in post response created At

--- src/main/java/com/webapp/madrasati/school_group/service/impl/CreatePostService.java
+++ src/main/java/com/webapp/madrasati/school_group/service/impl/CreatePostService.java
@@ -26,75 +26,78 @@ import com.webapp.madrasati.school_group.repository.GroupRepository;
 
 @Service
 public class CreatePostService {
-    private final GroupPostRepository groupPostRepository;
-    private final GroupRepository groupRepository;
-    private final ImagePostRepository imagePostRepository;
-    private final UserIdSecurity userId;
-    private final LocalFileStorageService fileStorageService;
+        private final GroupPostRepository groupPostRepository;
+        private final GroupRepository groupRepository;
+        private final ImagePostRepository imagePostRepository;
+        private final UserIdSecurity userId;
+        private final LocalFileStorageService fileStorageService;
 
-    public CreatePostService(GroupPostRepository groupPostRepository, GroupRepository groupRepository,
-            UserIdSecurity userIdSecurity, ImagePostRepository imagePostRepository,
-            LocalFileStorageService fileStorageService) {
-        this.fileStorageService = fileStorageService;
-        this.userId = userIdSecurity;
-        this.groupPostRepository = groupPostRepository;
-        this.groupRepository = groupRepository;
-        this.imagePostRepository = imagePostRepository;
-    }
-
-    @Async("taskExecutor")
-    @Transactional
-    public CompletableFuture<PostResponseBodyDto> createPost(List<MultipartFile> files, String caption,
-            String groupIdString) {
-
-        ObjectId groupId = new ObjectId(groupIdString);
-        Group group = groupRepository.findById(groupId)
-                .orElseThrow(() -> new ResourceNotFoundException("Group not found"));
-
-
-        GroupPost post = groupPostRepository.save(
-                GroupPost.builder()
-                        .groupId(groupId)
-                        .caption(caption)
-                        .authorId(userId.getUId()).build());
-
-        String className = "group";
-        String category = "post/" + post.getId().toString();
-        try {
-            List<ImagePost> imagesPost = new ArrayList<>();
-            if (files != null && !files.isEmpty()) {
-                List<String> fileNames = fileStorageService.storeFiles(className, groupIdString, category , files);
-
-                imagesPost = fileNames.stream()
-                        .map(fileName -> ImagePost.builder()
-                                .imageName(fileName)
-                                .imagePath(
-                                        fileStorageService.getFileUrl(className, groupIdString, category, fileName))
-                                .build())
-                        .toList();
-
-                imagePostRepository.saveAll(imagesPost);
-            }
-
-            post.setImagePost(imagesPost.stream()
-                    .map(BaseCollection::getId).toList());
-
-            groupPostRepository.save(post);
-
-            group.getGroupPostIds().add(post.getId());
-
-            groupRepository.save(group);
-
-            PostResponseBodyDto postResponseBodyDto = PostResponseBodyDto.builder()
-                    .authorId(post.getAuthorId())
-                    .caption(post.getCaption())
-                    .imagePost(post.getImagePost())
-                    .build();
-
-            return CompletableFuture.completedFuture(postResponseBodyDto);
-
-        } catch (Exception e) {
-            throw new InternalServerErrorException("Something went wrong while creating post", e);
+        public CreatePostService(GroupPostRepository groupPostRepository, GroupRepository groupRepository,
+                        UserIdSecurity userIdSecurity, ImagePostRepository imagePostRepository,
+                        LocalFileStorageService fileStorageService) {
+                this.fileStorageService = fileStorageService;
+                this.userId = userIdSecurity;
+                this.groupPostRepository = groupPostRepository;
+                this.groupRepository = groupRepository;
+                this.imagePostRepository = imagePostRepository;
         }
-    }
+
+        @Async("taskExecutor")
+        @Transactional
+        public CompletableFuture<PostResponseBodyDto> createPost(List<MultipartFile> files, String caption,
+                        String groupIdString) {
+                boolean withImage = false;
+                ObjectId groupId = new ObjectId(groupIdString);
+                Group group = groupRepository.findById(groupId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Group not found"));
+
+                GroupPost post = groupPostRepository.save(
+                                GroupPost.builder()
+                                                .groupId(groupId)
+                                                .caption(caption)
+                                                .authorId(userId.getUId()).build());
+
+                String className = "group";
+                String category = "post/" + post.getId().toString();
+                try {
+                        List<ImagePost> imagesPost = new ArrayList<>();
+                        if (files != null && !files.isEmpty()) {
+                                List<String> fileNames = fileStorageService.storeFiles(className, groupIdString,
+                                                category, files);
+
+                                imagesPost = fileNames.stream()
+                                                .map(fileName -> ImagePost.builder()
+                                                                .imageName(fileName)
+                                                                .imagePath(
+                                                                                fileStorageService.getFileUrl(className,
+                                                                                                groupIdString, category,
+                                                                                                fileName))
+                                                                .build())
+                                                .toList();
+
+                                imagePostRepository.saveAll(imagesPost);
+                                withImage = true;
+                        }
+
+                        post.setImagePost(imagesPost.stream()
+                                        .map(BaseCollection::getId).toList());
+                        groupPostRepository.save(post);
+
+                        group.getGroupPostIds().add(post.getId());
+
+                        groupRepository.save(group);
+
+                        PostResponseBodyDto postResponseBodyDto = PostResponseBodyDto.builder()
+                                        .authorId(post.getAuthorId())
+                                        .caption(post.getCaption())
+                                        .imagePost(post.getImagePost())
+                                        .withImage(withImage)
+                                        .build();
+
+                        return CompletableFuture.completedFuture(postResponseBodyDto);
+
+                } catch (Exception e) {
+                        throw new InternalServerErrorException("Something went wrong while creating post", e);
+                }
+        }
 }
