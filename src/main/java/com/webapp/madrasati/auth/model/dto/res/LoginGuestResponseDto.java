12fb783029f88519e9aa7d46a1a36232e2a1478b
sameer11001 <sameeralhajeid.prog@gmail.com>
2024-11-05 22:39:13 +0300

    fix the guest duplicate issue and for mongo drop database for dev profile and increase the rate limiter and fix guest login with data

--- src/main/java/com/webapp/madrasati/auth/model/dto/res/LoginGuestResponseDto.java
+++ src/main/java/com/webapp/madrasati/auth/model/dto/res/LoginGuestResponseDto.java
@@ -3,10 +3,12 @@ package com.webapp.madrasati.auth.model.dto.res;
 import java.time.Instant;
 import java.util.UUID;
 
+import com.webapp.madrasati.school.repository.summary.SchoolSummary;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 @Data
 @Builder
@@ -23,4 +25,8 @@ public class LoginGuestResponseDto {
     private String token;
 
     Instant expiryDate;
+
+    Page<SchoolSummary> data;
+
+
 }
---
+++ src/main/java/com/webapp/madrasati/school_group/service/impl/MongoDbDrop.java
@@ -0,0 +1,26 @@
+package com.webapp.madrasati.school_group.service.impl;
+
+import com.webapp.madrasati.core.config.LoggerApp;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
+public class MongoDbDrop {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public MongoDbDrop(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @PostConstruct
+    public void dropDatabaseAtStartup() {
+        mongoTemplate.getDb().drop();
+        LoggerApp.debug("Database dropped successfully at startup in development environment!");
+    }
+}
